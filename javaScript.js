1f4ced75017552198e7500dd8cf7dc4af6eaab92
vijaykunigiri17 <vijaykunigiri17@gmail.com>
2025-04-24 15:48:53 +0530

    Ready To Commit Guys

---
+++ javaScript.js
@@ -0,0 +1,16 @@
+const tag=document.createElement("h1")
+tag.innerHTML="My Self Vijay Kumar From Kurnool"
+tag.style.color="white"
+tag.style.textAlign="center"
+const root=document.getElementById("root")
+root.append(tag)
+const bd=document.getElementById("bd")
+bd.style.backgroundColor="black"
+const newtag=document.createElement("div")
+newtag.style.height="200px"
+newtag.style.width="200px"
+newtag.style.borderRadius="50%"
+newtag.style.backgroundColor="orange"
+newtag.style.border="5px solid white"
+newtag.style.marginLeft="650px"
+bd.append(newtag)
